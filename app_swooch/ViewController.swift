b66f388d84d0eb00024e0e8af40904460228fe75
Amr Mohamed <amro.mohamed@seamlabs.com>
2020-07-05 23:46:19 +0200

    first commit

---
+++ app_swooch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app_swooch
+//
+//  Created by admin on 7/5/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
