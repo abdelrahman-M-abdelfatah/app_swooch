60309919886b8fe6207155e5390017275aa50d05
abdelrahman-M-abdelfatah <abdoabdo11223399@gmail.com>
2020-07-09 06:44:04 +0200

    4 edition

--- app_swooch/ViewController.swift
+++ app_swooch/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var bgimg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        swooshlogo.frame = CGRect(x: view.frame.size.width/2 - swooshlogo.frame.size.width/2, y: 50, width: swooshlogo.frame.size.width, height: swooshlogo.frame.size.height)
-        bgimg.frame = view.frame
+       
      
     }
 
